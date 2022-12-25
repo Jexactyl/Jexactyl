93daa1973aa6a46b7c278010aecab33250d22a86
cmrxnn <cmrxn2005@gmail.com>
2022-12-25 10:51:33 -0800

    Fix server name showing up in headers

--- resources/scripts/components/elements/ServerContentBlock.tsx
+++ resources/scripts/components/elements/ServerContentBlock.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import { ServerContext } from '@/state/server';
 import PageContentBlock, { PageContentBlockProps } from '@/components/elements/PageContentBlock';
 
 interface Props extends PageContentBlockProps {
     title: string;
 }
 
-const ServerContentBlock: React.FC<Props> = ({ title, children, ...props }) => {
-    const name = ServerContext.useStoreState((state) => state.server.data!.name);
-
-    return (
-        <PageContentBlock title={`${name} | ${title}`} {...props}>
-            {children}
-        </PageContentBlock>
-    );
-};
+const ServerContentBlock: React.FC<Props> = ({ title, children, ...props }) => (
+    <PageContentBlock title={title} {...props}>
+        {children}
+    </PageContentBlock>
+);
 
 export default ServerContentBlock;
