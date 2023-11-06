b8378ef22681ddd5f892589463ddfda54628cbe7
Cam White <72230943+cmrxnn@users.noreply.github.com>
2023-11-06 12:57:05 +0000

    Update MakeNodeCommand.php

--- app/Console/Commands/Node/MakeNodeCommand.php
+++ app/Console/Commands/Node/MakeNodeCommand.php
@@ -61,7 +61,7 @@ class MakeNodeCommand extends Command
         $data['upload_size'] = $this->option('uploadSize') ?? $this->ask('Enter the maximum filesize upload', '100');
         $data['daemonListen'] = $this->option('daemonListeningPort') ?? $this->ask('Enter the wings listening port', '8080');
         $data['daemonSFTP'] = $this->option('daemonSFTPPort') ?? $this->ask('Enter the wings SFTP listening port', '2022');
-        $data['daemonBase'] = $this->option('daemonBase') ?? $this->ask('Enter the base folder', '/var/lib/jexactyl/volumes');
+        $data['daemonBase'] = $this->option('daemonBase') ?? $this->ask('Enter the base folder', '/var/lib/pterodactyl/volumes');
 
         $node = $this->creationService->handle($data);
         $this->line('Successfully created a new node on the location ' . $data['location_id'] . ' with the name ' . $data['name'] . ' and has an id of ' . $node->id . '.');
