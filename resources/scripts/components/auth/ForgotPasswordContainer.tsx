17056ad4dee9d76d3ffd8e24485180e6135a3b2c
Edgar Salazar <edgargarza076@gmail.com>
2026-07-12 21:02:49 -0600

    Fix invisible reCAPTCHA token check

--- resources/scripts/components/auth/ForgotPasswordContainer.tsx
+++ resources/scripts/components/auth/ForgotPasswordContainer.tsx
@@ -39,7 +39,7 @@ function ForgotPasswordContainer() {
 
         // If there is no token in the state yet, request the token and then abort this submit request
         // since it will be re-submitted when the recaptcha data is returned by the component.
-        if (recaptchaEnabled && !token) {
+        if (recaptchaEnabled && !token.current) {
             ref.current!.execute().catch(error => {
                 console.error(error);
 
