13e4345a8822958989a523d5c9958113b3efef74
CraftrDoinTech <84291170+CraftrDoinTech@users.noreply.github.com>
2026-08-11 11:42:12 -0700

    Added tax_code
    for proper taxing

--- app/Services/Billing/PaymentService.php
+++ app/Services/Billing/PaymentService.php
@@ -13,11 +13,13 @@ class PaymentService
      */
     public function create(StripeClient $stripe, User $user, Product $product, array $metadata, ?float $price = null, ?float $deploymentFee = null): object
     {
+        $taxCode = $isBusiness ? 'txcd_10101000' : 'txcd_10010001'; // IaaS - business / personal use
         $lineItems = [[
             'price_data' => [
                 'currency' => strtolower(config('modules.billing.currency.code')),
                 'product_data' => [
                     'name' => $product->name,
+                    'tax_code' => $taxCode,
                 ],
                 'unit_amount' => (int) round(($price ?? $product->price) * 100),
             ],
