047ad7c593f55977833f417967fdd0eebe359b65
CraftrDoinTech <84291170+CraftrDoinTech@users.noreply.github.com>
2026-08-16 15:10:25 -0700

    Removed stripe version
    version change required in dashboard

--- app/Services/Billing/PaymentService.php
+++ app/Services/Billing/PaymentService.php
@@ -52,8 +52,6 @@ class PaymentService
             'cancel_url' => config('app.url') . '/account/billing/cancel',
 
             'metadata' => $metadata,
-        ], [
-            'stripe_version' => '2025-03-31'
         ]);
 
         return $transaction;
