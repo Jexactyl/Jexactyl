acbfb95efdcb24d90a61b3f3cde195af7ff427eb
CraftrDoinTech <84291170+CraftrDoinTech@users.noreply.github.com>
2026-08-18 09:13:05 -0700

    Update OrderContainer.tsx

--- resources/scripts/components/account/billing/order/OrderContainer.tsx
+++ resources/scripts/components/account/billing/order/OrderContainer.tsx
@@ -338,7 +338,6 @@ export default () => {
                                             />
                                         </div>
                                     )}
-                                    </div>
                                 </TitledGreyBox>
                             </div>
                         </div>
