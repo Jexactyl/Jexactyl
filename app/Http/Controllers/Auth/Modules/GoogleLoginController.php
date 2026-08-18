5927ca45f62a2ce288f001f8c07cf6c06bcbbf3d
camwhi-te <cmrxn2005@gmail.com>
2026-08-18 22:43:53 +0100

    Run PHP-CS

--- app/Http/Controllers/Auth/Modules/GoogleLoginController.php
+++ app/Http/Controllers/Auth/Modules/GoogleLoginController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Everest\Exceptions\DisplayException;
 use Laravel\Socialite\Facades\Socialite;
-use Everest\Http\Controllers\Auth\AbstractLoginController;
 use Everest\Extensions\Laravel\Socialite\GoogleProvider;
+use Everest\Http\Controllers\Auth\AbstractLoginController;
 
 class GoogleLoginController extends AbstractLoginController
 {
