b3a1c6482d87aa90a2f4dec61e7d6b21a979f8e0
CraftrDoinTech <84291170+CraftrDoinTech@users.noreply.github.com>
2026-08-19 10:27:37 -0700

    Update ServerDeploymentService.php

--- app/Services/Billing/ServerDeploymentService.php
+++ app/Services/Billing/ServerDeploymentService.php
@@ -30,17 +30,17 @@ class ServerDeploymentService
      *
      * @throws NoViableAllocationException
      */
-    public function handle(User $user, Product $product, array $metadata, Order $order): Server
+    public function handle(User $user, Product $product, object $metadata, Order $order): Server
     {
         $renewalDays = config('modules.billing.renewal.days', 30);
-        $metadataEggId = $metadata['egg_id'] ?? '';
+        $metadataEggId = $metadata->egg_id ?? '';
         $egg = $this->resolveEgg($product, $metadataEggId !== '' ? (int) $metadataEggId : null);
-        $allocation = $this->getAllocation($metadata['node_id'], $order->id);
-        $environment = $this->getEnvironment($egg->id, json_decode($metadata['variables']));
+        $allocation = $this->getAllocation($metadata->node_id, $order->id);
+        $environment = $this->getEnvironment($egg->id, json_decode($metadata->variables));
 
         try {
             $server = $this->creation->handle([
-                'node_id' => $metadata['node_id'],
+                'node_id' => $metadata->node_id,
                 'allocation_id' => $allocation,
                 'egg_id' => $egg->id,
                 'nest_id' => $product->category->nest_id,
