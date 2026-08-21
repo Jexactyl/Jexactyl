807f5a393980ed56b4a04c5413aa4b5860a0a865
CraftrDoinTech <84291170+CraftrDoinTech@users.noreply.github.com>
2026-08-21 09:45:35 -0700

    fixed route check

--- app/Http/Middleware/VerifyReCaptcha.php
+++ app/Http/Middleware/VerifyReCaptcha.php
@@ -31,7 +31,7 @@ class VerifyReCaptcha
         $recaptchaResponse = $request->input('g-recaptcha-response');
         $recaptchaState = $request->input('state');
 
-        if ($recaptchaState && (str_starts_with($recaptchaState, 'discord-') || $request->is('login/google/callback') || $request->routeIs('auth.google.callback'))) {
+        if ($recaptchaState && (str_starts_with($recaptchaState, 'discord-') || $request->is('auth/modules/google/authenticate') || $request->routeIs('auth.modules.google.authenticate'))) {
             return $next($request);
         }
         
